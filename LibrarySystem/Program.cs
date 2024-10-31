e33ac0c4a9c2abc3ca0b42959d7aa1ac568e0325
YOS PISETH <201002@student.sru.edu.kh>
2024-10-31 22:07:24 +0700

    Add project files.

---
+++ LibrarySystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using LibrarySystem.Views;
+using System;
+using System.Windows.Forms;
+
+namespace LibrarySystem
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Main());
+		}
+	}
+}
